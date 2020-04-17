aa59821730734cf4be28ed34e84deb9d87c87aa0
ssalteko <ssalteko@gmail.com>
2020-04-17 13:35:02 -0700

    sadf

--- MIT GRAPHING RESULTS/MIT_Stage_Uniformity_plotting.py
+++ MIT GRAPHING RESULTS/MIT_Stage_Uniformity_plotting.py
@@ -4,7 +4,7 @@ from MIT_Stage_Uniformity_pandas import *
 
 def plot_diff_column_values(df,columns):
     ''' plots a given column of a given dataframe. '''
-    # df = add_diff_columns(df,columns)
+    # df = add_diff_columns(df,columns) 
     print(df)
     ax1 = df.iloc[20:-20].plot( x = 'Time', y = f'{columns[0]}diff', kind = 'scatter', color = 'red', label = f'{columns[0]}diff')
     
