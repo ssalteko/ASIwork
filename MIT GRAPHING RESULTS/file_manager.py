31ef7a919e05fca2191f222231cab9119e952734
ssalteko <ssalteko@gmail.com>
2020-04-22 19:16:49 -0700

    formatting again

--- MIT GRAPHING RESULTS/file_manager.py
+++ MIT GRAPHING RESULTS/file_manager.py
@@ -31,15 +31,15 @@ def get_file_info_dict(file):
     with open(file, 'r') as file:
         lines = file.readlines()
     
-    fileInfo = lines[0]
-    fileInfoDict = json.loads(fileInfo)
+    file_info = lines[0]
+    file_info_dict = json.loads(file_info)
 
-    return fileInfoDict
+    return file_info_dict
 
 
 
 def get_data_list(directory):
-    '''returns a list of txt files in a directory: "directory"'''
+    '''returns a list of txt files in a directory.'''
     
     files = os.listdir(directory)
     data_files = []
